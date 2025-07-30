f61ea65d6a50034b2ceda91efdb8cab7c6719271
Motojima <r.motojima5@gmail.com>
2025-07-30 23:26:38 +0900

    RuboCopのエラー修正

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -38,7 +38,7 @@ class ArticlesController < ApplicationController
     def destroy
         article = Article.find(params[:id])
         article.destroy!
-        redirect_to root_path, status: :see_other, notice: '削除に成功しました'
+        redirect_to root_path, status: :see_other, notice: "削除に成功しました"
     end
 
     private
