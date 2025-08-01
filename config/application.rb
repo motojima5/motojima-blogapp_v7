4cc402555b14a4519b597a2159b4d8ea24e6b6cd
Motojima <r.motojima5@gmail.com>
2025-08-01 18:16:23 +0900

    動画外の修正

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,10 @@ require_relative 'boot'
 
 require 'rails/all'
 
+if ['development', 'test'].include? ENV['RAILS_ENV']
+  Dotenv::Railtie.load
+end
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
