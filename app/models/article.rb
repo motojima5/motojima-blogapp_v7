e2f54886ebf2643b70661a2aa80509903ed6e1a9
Motojima <r.motojima5@gmail.com>
2025-08-12 11:53:34 +0900

    content カラムを排除

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
     has_one_attached :eyecatch
+    has_rich_text :content
 
     validates :title, presence: true
     validates :title, length: { minimum: 2, maximum: 100 }
