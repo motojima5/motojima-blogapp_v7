29949e0e8e0894fc29ebc74a71bb38a9d98a47b5
Motojima <r.motojima5@gmail.com>
2025-08-12 15:50:53 +0900

    action text の設定 @import 'trix.css'が正しい

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,10 +7,6 @@ class Article < ApplicationRecord
     validates :title, format: { with: /\A(?!\@).*\z/ }
 
     validates :content, presence: true
-    validates :content, length: { minimum: 10 }
-    validates :content, uniqueness: true
-
-    validate :validate_title_and_content_length
 
     has_many :comments, dependent: :destroy
     has_many :likes, dependent: :destroy
@@ -26,13 +22,5 @@ class Article < ApplicationRecord
 
     def like_count
         likes.count
-    end
-
-    private
-    def validate_title_and_content_length
-        char_count = self.title.length + self.content.length
-        unless char_count > 100
-            errors.add(:content, '100文字いじょうで！')
-        end
     end
 end
